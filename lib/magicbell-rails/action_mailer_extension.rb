a0427954c08c9ef99baf0e74c7856ee6b2d8b04a
Nisanth Chunduru <nisanth074@gmail.com>
2017-07-15 02:05:56 +0530

    Add #magicbell_notification_metadata, #magicbell_notification_skip, #magicbell_notification_title and #magicbell_notification_action_url methods

--- lib/magicbell-rails/action_mailer_extension.rb
+++ lib/magicbell-rails/action_mailer_extension.rb
@@ -1,7 +1,28 @@
 module MagicBellRails
   module ActionMailerExtension
-    def ring_the_magicbell
-      default bcc: MagicBellRails.magic_address
+    def self.included(mailer_class)
+      mailer_class.send :extend, ClassMethods
     end
-  end
+
+    module ClassMethods
+      def ring_the_magicbell
+        default bcc: MagicBellRails.magic_address
+      end
+    end
+
+    def magicbell_notification_metadata(metadata)
+      headers["X-MagicBell-Notification-Metadata"] = metadata.to_json
+    end
+
+    def magicbell_notification_skip
+      headers["X-MagicBell-Notification-Skip"] = metadata.to_json
+    end
+
+    def magicbell_notification_title(title)
+      headers["X-MagicBell-Notification-Title"] = title
+    end
+  
+    def magicbell_notification_action_url(action_url)
+      headers["X-MagicBell-Notification-ActionUrl"] = action_url
+    ende
 end
--- lib/magicbell-rails/railtie.rb
+++ lib/magicbell-rails/railtie.rb
@@ -3,7 +3,7 @@ require "magicbell-rails/action_mailer_extension"
 module MagicBellRails
   class Railtie < Rails::Railtie
     initializer 'magicbell-rails' do
-      ActionMailer::Base.send :extend, MagicBellRails::ActionMailerExtension
+      ActionMailer::Base.send :include, MagicBellRails::ActionMailerExtension
     end
   end
 end
