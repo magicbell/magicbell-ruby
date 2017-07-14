9df93559fe363b2d4e82785f101df23295b6ab2f
Nisanth Chunduru <nisanth074@gmail.com>
2017-07-14 18:02:45 +0530

    Remove valid_hmac? method (for now)
    ActiveSupport::SecurityUtils is only available in Rails 4 and above

--- lib/magicbell-rails/hmac.rb
+++ lib/magicbell-rails/hmac.rb
@@ -1,5 +1,4 @@
 require "base64"
-require "active_support/security_utils"
 
 module MagicBellRails
   module HMAC
@@ -7,10 +6,6 @@ module MagicBellRails
       def calculate_hmac(message, hmac_key)
         digest  = OpenSSL::Digest::Digest.new('sha256')
         Base64.encode64(OpenSSL::HMAC.digest(digest, hmac_key, message)).strip
-      end
-
-      def valid_hmac?(message, hmac_key, hmac)
-        ActiveSupport::SecurityUtils.secure_compare(calculate_hmac(message, hmac_key), hmac)
       end
     end
   end
