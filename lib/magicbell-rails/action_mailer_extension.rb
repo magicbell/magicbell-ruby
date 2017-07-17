dea6a8e13a9b659eabd4a75136f8abc8ad235e62
Nisanth Chunduru <nisanth074@gmail.com>
2017-07-17 21:27:52 +0530

    Add more missing specs and fix typo

--- lib/magicbell-rails/action_mailer_extension.rb
+++ lib/magicbell-rails/action_mailer_extension.rb
@@ -12,20 +12,20 @@ module MagicBellRails
       end
     end
 
-    def magicbell_notification_metadata(metadata)
-      headers["X-MagicBell-Notification-Metadata"] = metadata.to_json
+    def magicbell_notification_action_url(action_url)
+      headers["X-MagicBell-Notification-ActionUrl"] = action_url
     end
 
-    def magicbell_notification_skip
-      headers["X-MagicBell-Notification-Skip"] = metadata.to_json
+    def magicbell_notification_metadata(metadata)
+      headers["X-MagicBell-Notification-Metadata"] = metadata.to_json
     end
 
     def magicbell_notification_title(title)
       headers["X-MagicBell-Notification-Title"] = title
     end
-  
-    def magicbell_notification_action_url(action_url)
-      headers["X-MagicBell-Notification-ActionUrl"] = action_url
+
+    def magicbell_notification_skip
+      headers["X-MagicBell-Notification-Skip"] = true
     end
   end
 end
--- spec/magicbell-rails/action_mailer_extension_spec.rb
+++ spec/magicbell-rails/action_mailer_extension_spec.rb
@@ -1,25 +1,65 @@
 require "action_mailer"
 
-class NotificationMailer < ActionMailer::Base
-  include MagicBellRails::ActionMailerExtension
-
-  def new_comment
-    mail(
-      from: "notifications@mydummyapp.com",
-      to: "dummycustomer@example.com",
-      subject: "Just a dummy notification",
-      body: "Just a dummy notification"
-    )
-    magicbell_notification_metadata(comment_id: 1)
-  end
-end
-
 describe MagicBellRails::ActionMailerExtension do
   describe "Helper methods" do
+    let(:custom_action_url) { "https://myapp.com/comments/1" }
+    let(:metadata) { { comment_id: 1 } }
+    let(:custom_title) { "I'd like to have a title that is different from the email's subject" }
+    let(:notification_mailer) {
+      Class.new(ActionMailer::Base) do
+        include MagicBellRails::ActionMailerExtension
+
+        def new_comment
+          mail(
+            from: "notifications@mydummyapp.com",
+            to: "dummycustomer@example.com",
+            subject: "Just a dummy notification",
+            body: "Just a dummy notification"
+          )
+          custom_action_url = "https://myapp.com/comments/1"
+          magicbell_notification_action_url(custom_action_url)
+          magicbell_notification_metadata(comment_id: 1)
+          custom_title = "I'd like to have a title that is different from the email's subject"
+          magicbell_notification_title(custom_title)
+        end
+
+        def reaching_monthly_limit
+          mail(
+            from: "notifications@mydummyapp.com",
+            to: "dummycustomer@example.com",
+            subject: "Just a dummy notification",
+            body: "Just a dummy notification"
+          )
+          magicbell_notification_skip
+        end
+      end
+    }
+
+    describe "#magicbell_notification_action_url" do
+      it "adds the 'X-MagicBell-Notification-ActionUrl' header" do
+        mail = notification_mailer.new_comment
+        expect(mail["X-MagicBell-Notification-ActionUrl"].decoded).to eq(custom_action_url)
+      end
+    end
+
     describe "#magicbell_notification_metadata" do
-      it "does something" do
-        mail = NotificationMailer.new_comment
-        expect(mail["X-MagicBell-Notification-Metadata"].decoded).to eq({ comment_id: 1 }.to_json)
+      it "adds the 'X-MagicBell-Notification-Metadata' header" do
+        mail = notification_mailer.new_comment
+        expect(mail["X-MagicBell-Notification-Metadata"].decoded).to eq(metadata.to_json)
+      end
+    end
+
+    describe "#magicbell_notification_title" do
+      it "adds the 'X-MagicBell-Notification-Title' header" do
+        mail = notification_mailer.new_comment
+        expect(mail["X-MagicBell-Notification-Title"].decoded).to eq(custom_title)
+      end
+    end
+
+    describe "#magicbell_notification_skip" do
+      it "adds the 'X-MagicBell-Notification-Skip' header" do
+        mail = notification_mailer.reaching_monthly_limit
+        expect(mail["X-MagicBell-Notification-Skip"].decoded).to eq("true")
       end
     end
   end
