6d0c6ae02c78738ea08ded745305fc26a768292c
Nisanth Chunduru <nisanth074@gmail.com>
2020-12-02 17:16:27 +0530

    Fix failing rspec in spec/magicbell_spec.rb

--- spec/magicbell_spec.rb
+++ spec/magicbell_spec.rb
@@ -2,16 +2,15 @@ describe MagicBell do
   let(:api_secret) { "dummy_api_secret" }
 
   describe ".configure" do
-    let(:magic_address) { "dummy_magic_address@ring.magicbell.io" }
     let(:api_key) { "dummy_api_key" }
-    let(:project_id) { 1 }
+    let(:api_secret) { "dummy_api_secret" }
+    let(:bcc_email) { "dummy_magic_address@ring.magicbell.io" }
 
     before do
       MagicBell.configure do |config|
-        config.magic_address = magic_address
         config.api_key = api_key
         config.api_secret = api_secret
-        config.project_id = project_id
+        config.bcc_email = bcc_email
       end
     end
 
@@ -20,11 +19,9 @@ describe MagicBell do
     end
 
     it "configures the gem" do
-      expect(MagicBell.magic_address).to eq(magic_address)
       expect(MagicBell.api_key).to eq(api_key)
       expect(MagicBell.api_secret).to eq(api_secret)
       expect(MagicBell.api_secret).to eq(api_secret)
-      expect(MagicBell.api_host).to eq("https://api.example.com")
     end
   end
 
